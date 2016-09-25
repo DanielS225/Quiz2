01bb5e5d7fed82ddd8ab718cbfdd2f2bb9434c44
Daniel <Daniel@128.4.101.208>
2016-09-25 16:50:29 -0400

    initial commit

---
+++ Quiz2/src/quiz2/Tuition.java
@@ -0,0 +1,29 @@
+package quiz2;
+
+import java.util.Scanner;
+
+public class Tuition {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		/*The current tuition rate for 2014-2015 is $12,342 (not including room and board). 
+
+		Write a program that will calculate the total cost of tuition to complete a degree. 
+
+		Assumptions: 
+		1) You're a freshman, this is the first year you'll be paying tuition 
+		2) You plan to attend a standard four year degree. You don't drop out, take time off, etc. 
+		3) Each year there is an percentage increase (variable, based on university costs) 
+
+		Hint: 
+		Initial tuition cost and percentage increases should be variables read by Scanner. 
+
+		Skills used: 
+		Basic math, power function, loop (for loop), display formatted answer. */
+
+		scn.close();
+		
+	}
+
+}
